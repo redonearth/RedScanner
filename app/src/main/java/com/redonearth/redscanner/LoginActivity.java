f3614d2e272718f3ea5849aa19768fde6297d6e2
redonearth <wona23@gmail.com>
2019-06-11 15:11:27 +0900

    .gitignore fix

--- app/src/main/java/com/redonearth/redscanner/LoginActivity.java
+++ app/src/main/java/com/redonearth/redscanner/LoginActivity.java
@@ -2,7 +2,10 @@ package com.redonearth.redscanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView registerButton = findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
     }
 }
