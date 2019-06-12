a40c7308ba959a2688456bc8c6a66c27ed4225a5
redonearth <wona23@gmail.com>
2019-06-12 15:32:02 +0900

    로그인 기능 구현

---
+++ app/src/main/java/com/redonearth/redscanner/LoginRequest.java
@@ -0,0 +1,25 @@
+package com.redonearth.redscanner;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginRequest extends StringRequest {
+
+    final static private String URL = "http://10.0.0.35/UserLogin.php"; // localhost일 경우 작동 안 함. IP나 url 입력.
+    private Map<String, String> parameters;
+
+    public LoginRequest(String userID, String userPassword, Response.Listener<String> listener) {
+        super(Method.POST, URL, listener, null);
+        parameters = new HashMap<>();
+        parameters.put("userID", userID);
+        parameters.put("userPassword", userPassword);
+    }
+
+    @Override
+    protected Map<String, String> getParams() {
+        return parameters;
+    }
+}
