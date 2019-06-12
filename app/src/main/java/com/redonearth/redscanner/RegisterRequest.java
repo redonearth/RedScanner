dfe4df0a1610f9c17e0cb6c886a2a08ccd8da168
redonearth <wona23@gmail.com>
2019-06-12 14:57:08 +0900

    회원 가입 기능 구현

---
+++ app/src/main/java/com/redonearth/redscanner/RegisterRequest.java
@@ -0,0 +1,26 @@
+package com.redonearth.redscanner;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterRequest extends StringRequest {
+
+    final static private String URL = "http://10.0.0.35/UserRegister.php"; // localhost일 경우 작동 안 함. IP나 url 입력.
+    private Map<String, String> parameters;
+
+    public RegisterRequest(String userID, String userPassword, String userEmail, Response.Listener<String> listener) {
+        super(Method.POST, URL, listener, null);
+        parameters = new HashMap<>();
+        parameters.put("userID", userID);
+        parameters.put("userPassword", userPassword);
+        parameters.put("userEmail", userEmail);
+    }
+
+    @Override
+    protected Map<String, String> getParams() {
+        return parameters;
+    }
+}
---
+++ app/src/main/java/com/redonearth/redscanner/ValidateRequest.java
@@ -0,0 +1,24 @@
+package com.redonearth.redscanner;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidateRequest extends StringRequest {
+
+    final static private String URL = "http://10.0.0.35/UserValidate.php"; // localhost일 경우 작동 안 함. IP나 url 입력.
+    private Map<String, String> parameters;
+
+    public ValidateRequest(String userID, Response.Listener<String> listener) {
+        super(Method.POST, URL, listener, null);
+        parameters = new HashMap<>();
+        parameters.put("userID", userID);
+    }
+
+    @Override
+    protected Map<String, String> getParams() {
+        return parameters;
+    }
+}
