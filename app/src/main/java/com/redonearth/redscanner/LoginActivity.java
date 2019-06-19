ea79a0dc80a40c68c79d430a3ca6090cb3ca780c
redonearth <wona23@gmail.com>
2019-06-19 18:01:21 +0900

    자동 로그인 추가 진행 중

--- app/src/main/java/com/redonearth/redscanner/LoginActivity.java
+++ app/src/main/java/com/redonearth/redscanner/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.redonearth.redscanner;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -20,6 +22,7 @@ import org.json.JSONObject;
 public class LoginActivity extends AppCompatActivity {
 
     private AlertDialog dialog;
+    String loginId, loginPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,48 +42,68 @@ public class LoginActivity extends AppCompatActivity {
         final EditText idText = findViewById(R.id.idText);
         final EditText passwordText = findViewById(R.id.passwordText);
         final Button loginButton = findViewById(R.id.loginButton);
+        SharedPreferences auto = getSharedPreferences("auto", Activity.MODE_PRIVATE);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        // 처음에는 SharedPreferences에 아무런 정보도 없으므로 값을 저장할 키들을 생성한다.
+        // getString의 첫 번째 인자는 저장될 키, 두 번째 인자는 값.
+        loginId = auto.getString("idText", null);
+        loginPassword = auto.getString("passwordText", null);
 
-            @Override
-            public void onClick(View view) {
-                String userID = idText.getText().toString();
-                String userPassword = passwordText.getText().toString();
+        if(loginId != null && loginPassword != null) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+            dialog = builder.setMessage("자동 로그인 되었습니다.")
+                    .setPositiveButton("확인", null)
+                    .create();
+            dialog.show();
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            LoginActivity.this.startActivity(intent);
+            finish();
+        } else if(loginId == null && loginPassword == null) {
+            loginButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String userID = idText.getText().toString();
+                    String userPassword = passwordText.getText().toString();
+                    Response.Listener<String> responseListener = new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                JSONObject jsonResponse = new JSONObject(response);
+                                Log.d("jsonResponse :::::::", jsonResponse.toString());
+                                boolean success = jsonResponse.getBoolean("success");
+                                if(success) {
+                                    SharedPreferences auto = getSharedPreferences("auto", Activity.MODE_PRIVATE);
+                                    SharedPreferences.Editor autoLogin = auto.edit();
+                                    autoLogin.putString("loginId", idText.getText().toString());
+                                    autoLogin.putString("loginPassword", passwordText.getText().toString());
+                                    autoLogin.apply();
 
-                Response.Listener<String> responseListener = new Response.Listener<String>() {
-
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject jsonResponse = new JSONObject(response);
-                            Log.d("jsonResponse :::::::", jsonResponse.toString());
-                            boolean success = jsonResponse.getBoolean("success");
-                            if(success) {
-                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
-                                dialog = builder.setMessage("로그인에 성공했습니다.")
-                                        .setPositiveButton("확인", null)
-                                        .create();
-                                dialog.show();
-                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                                LoginActivity.this.startActivity(intent);
-                                finish();
-                            } else {
-                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
-                                dialog = builder.setMessage("계정을 다시 확인하세요.")
-                                        .setNegativeButton("다시 시도", null)
-                                        .create();
-                                dialog.show();
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                    dialog = builder.setMessage("로그인에 성공했습니다.")
+                                            .setPositiveButton("확인", null)
+                                            .create();
+                                    dialog.show();
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    LoginActivity.this.startActivity(intent);
+                                    finish();
+                                } else {
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                    dialog = builder.setMessage("계정을 다시 확인하세요.")
+                                            .setNegativeButton("다시 시도", null)
+                                            .create();
+                                    dialog.show();
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace();
                             }
-                        } catch (Exception e) {
-                            e.printStackTrace();
                         }
-                    }
-                };
-                LoginRequest loginRequest = new LoginRequest(userID, userPassword, responseListener);
-                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
-                queue.add(loginRequest);
-            }
-        });
+                    };
+                    LoginRequest loginRequest = new LoginRequest(userID, userPassword, responseListener);
+                    RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                    queue.add(loginRequest);
+                }
+            });
+        }
     }
 
     @Override
