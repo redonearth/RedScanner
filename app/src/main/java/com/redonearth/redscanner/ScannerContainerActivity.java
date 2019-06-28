c447ff27278bf9b44c27eab3414284caaedec3e5
홍지상 <wona23@pomtec.co.kr>
2019-06-28 16:01:28 +0900

    스캔, 웹뷰 동작 관련 코드 수정

--- app/src/main/java/com/redonearth/redscanner/ScannerContainerActivity.java
+++ app/src/main/java/com/redonearth/redscanner/ScannerContainerActivity.java
@@ -1,58 +1,67 @@
 package com.redonearth.redscanner;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
-
-import java.util.regex.Pattern;
 
 public class ScannerContainerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setBeepEnabled(false); // 바코드 인식 시 소리
+        integrator.setOrientationLocked(false); // default가 세로 모드인데 휴대폰 방향에 따라 가로, 세로로 자동 변경됩니다.
+        integrator.setPrompt("바코드/QR코드를 사각형에 맞춰주세요!");  // 작동 안함... 왜지?
+        integrator.setCaptureActivity(ScannerActivity.class);
+        integrator.initiateScan();
     }
 
     protected void onResume() {
         super.onResume();
-
-        IntentIntegrator integrator = new IntentIntegrator(this);
-        integrator.setCaptureActivity(ScannerActivity.class);
-        integrator.setOrientationLocked(false); // default가 세로 모드인데 휴대폰 방향에 따라 가로, 세로로 자동 변경됩니다.
-        integrator.setPrompt("바코드/QR코드를 사각형에 맞춰주세요!");  // 작동 안함... 왜지?
-        integrator.initiateScan();
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, final Intent data) {
 
         if (requestCode == IntentIntegrator.REQUEST_CODE) {
             // QR코드/바코드를 스캔한 결과 값을 가져옵니다.
             IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+            new AlertDialog.Builder(this)
+                    .setTitle(R.string.app_name)
+                    .setMessage(result.getContents() + " [" + result.getFormatName() + "]")
+                    .setNegativeButton("확인", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                            startActivity(new Intent(ScannerContainerActivity.this, ScannerContainerActivity.class));
+                        }
+                    })
+                    .setPositiveButton("URL 이동", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            Intent intent = new Intent(ScannerContainerActivity.this, WebViewActivity.class);
+                            String contents = data.getStringExtra("SCAN_RESULT");
+                            intent.setData(Uri.parse(contents));
+                            startActivity(intent);
+                        }
+                    })
+                    .show();
 
             if (result.getContents() == null) {
                 // 취소됨
                 Toast.makeText(this, "취소되었습니다.", Toast.LENGTH_LONG).show();
-            } else {
-                // 스캔된 QR Code --> result.getContents()
-                Toast.makeText(this, "스캔: " + result.getContents(), Toast.LENGTH_LONG).show();
-                String contents = data.getStringExtra("SCAN_RESULT");
-                if (Pattern.matches("[0-9]{1,13}", contents)) {
-
-                } else {
-                    Intent intent = new Intent(this, WebViewActivity.class);
-                    intent.setData(Uri.parse(contents));
-                    startActivity(intent);
-                }
             }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
         }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
--- app/src/main/java/com/redonearth/redscanner/WebViewActivity.java
+++ app/src/main/java/com/redonearth/redscanner/WebViewActivity.java
@@ -1,38 +1,87 @@
 package com.redonearth.redscanner;
 
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.MailTo;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class WebViewActivity extends Activity implements View.OnClickListener {
+import androidx.core.content.ContextCompat;
+
+public class WebViewActivity extends Activity {
 
     private WebView webView;
-    private EditText address;
+    private boolean isLoading = false;
 
-    @SuppressLint("SetJavaScriptEnabled")
+    private ImageButton mBackButton;
+    private ImageButton mForwardButton;
+    private ImageButton mRefreshButton;
+    private ImageButton mEnterButton;
+    private EditText mUrlBar;
+    private ProgressBar mProgressBar;
+
+    private TextView mCloseButton;
+
+    @SuppressLint({"SetJavaScriptEnabled"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
 
         webView = findViewById(R.id.webView);
-        address = findViewById(R.id.address_line);
 
-        webView.setWebViewClient(new WebViewClient() {
+        mForwardButton = findViewById(R.id.forward_btn);
+        mBackButton = findViewById(R.id.back_btn);
+        mRefreshButton = findViewById(R.id.refresh_btn);
+        mEnterButton = findViewById(R.id.enter_btn);
+        mUrlBar = findViewById(R.id.url_bar);
+        mProgressBar = findViewById(R.id.progress_bar);
+        mCloseButton = findViewById(R.id.close_btn);
+
+        mForwardButton.setOnClickListener(onClick);
+        mBackButton.setOnClickListener(onClick);
+        mRefreshButton.setOnClickListener(onClick);
+        mEnterButton.setOnClickListener(onClick);
+
+        mCloseButton.setOnClickListener(onClick);
+
+        mUrlBar.setOnKeyListener(new View.OnKeyListener() {
             @Override
-            public void onPageFinished(WebView view, String url) {
-                address.setText(url);
-                super.onPageFinished(view, url);
+            public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
+                if ((keyEvent.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    mEnterButton.performClick();
+                }
+                return false;
             }
         });
-        webView.setWebChromeClient(new WebChromeClient());
+
+        webView.setWebViewClient(new webClient());
+
+        /*
+         * Progress Bar
+         */
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                mProgressBar.setProgress(newProgress);
+            }
+        });
 
 //        WebSettings webSettings = webView.getSettings();
         /*
@@ -55,24 +104,104 @@ public class WebViewActivity extends Activity implements View.OnClickListener {
         webView.loadUrl(String.valueOf(uri));
     }
 
-    @Override
-    public void onClick(View view) {
-        switch(view.getId()){
-            case R.id.back_btn:
-                webView.goBack();
-                break;
+    private View.OnClickListener onClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.enter_btn:
+                    String url;
+                    url = mUrlBar.getText().toString();
+                    webView.loadUrl(url);
+                    break;
+                case R.id.refresh_btn:
+                    if (isLoading) {
+                        webView.stopLoading();
+                    } else {
+                        webView.reload();
+                    }
+                    break;
+                case R.id.back_btn:
+                    if (webView.canGoBack()) {
+                        webView.goBack();
+                    }
+                    break;
+                case R.id.forward_btn:
+                    if (webView.canGoForward()) {
+                        webView.goForward();
+                    }
+                    break;
+                case R.id.close_btn:
+                    startActivity(new Intent(WebViewActivity.this, ScannerContainerActivity.class));
+            }
+        }
+    };
 
-            case R.id.forward_btn:
-                webView.goForward();
-                break;
+    class webClient extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            mUrlBar.setText(url);
+            mRefreshButton.setBackground(ContextCompat.getDrawable(WebViewActivity.this, R.drawable.ic_block_white_28dp));
+            mRefreshButton.setPadding(0, 0, 0, 0);
+            mProgressBar.setVisibility(View.VISIBLE);
+            isLoading = true;
+        }
 
-            case R.id.refresh_btn:
-                String add = address.getText().toString();
-                if(!add.contains("http://")) {
-                    add = "http://" + add;
-                }
-                webView.loadUrl(add);
-                break;
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            mRefreshButton.setBackground(ContextCompat.getDrawable(WebViewActivity.this, R.drawable.ic_refresh_white_28dp));
+            mRefreshButton.setPadding(0, 0, 0, 0);
+            mProgressBar.setVisibility(View.GONE);
+            isLoading = false;
+        }
+
+        @Override
+        public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+            super.onReceivedError(view, request, error);
+        }
+
+        /*
+         * URL 타입별 Url Loading 방식 설정
+         */
+        @TargetApi(Build.VERSION_CODES.N)
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            String url = request.getUrl().toString();
+
+            if (url.startsWith("tel:")) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(url));
+                startActivity(intent);
+            } else if (url.startsWith("sms:")) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.parse(url));
+                startActivity(intent);
+            } else if (url.startsWith("mailto:")) {
+                MailTo mailTo = MailTo.parse(url);
+                Intent intent = newEmailIntent(WebViewActivity.this, mailTo.getTo(), mailTo.getSubject(), mailTo.getBody(), mailTo.getCc());
+                startActivity(intent);
+            } else if (url.endsWith(".mp3")) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setDataAndType(Uri.parse(url), "audio/*");
+                view.getContext().startActivity(intent);
+            } else if (url.endsWith(".mp4") || url.endsWith(".3gp")) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setDataAndType(Uri.parse(url), "video/*");
+                view.getContext().startActivity(intent);
+            } else if (url.startsWith("http:") || url.startsWith("https:")) {
+                view.loadUrl(url);
+            }
+            return true;
+        }
+
+        private Intent newEmailIntent(Context context, String address, String subject, String body, String cc) {
+            Intent intent = new Intent(Intent.ACTION_SEND);
+
+            intent.putExtra(Intent.EXTRA_EMAIL, new String[] { address });
+            intent.putExtra(Intent.EXTRA_TEXT, body);
+            intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+            intent.putExtra(Intent.EXTRA_CC, cc);
+            intent.setType("message/rfc822");
+            return intent;
         }
     }
 
@@ -83,11 +212,12 @@ public class WebViewActivity extends Activity implements View.OnClickListener {
 
         // 뒤로 가기 누를 때 동작
         if (webView.getOriginalUrl().equalsIgnoreCase(String.valueOf(uri))) {
-            super.onBackPressed();
+            startActivity(new Intent(this, ScannerContainerActivity.class));
         } else if (webView.canGoBack()) {
             webView.goBack();
         } else {
-            super.onBackPressed();
+            startActivity(new Intent(this, ScannerContainerActivity.class));
         }
+        super.onBackPressed();
     }
 }
